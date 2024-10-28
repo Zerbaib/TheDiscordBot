e8748faad8c5b4182f6aa5b2202e8529af5ff485
Hugo Hennetin <hugo.hennetin@proton.me>
2024-10-28 16:41:48 +0100

    leveling sys start

--- src/modules/ranking/sysRank.py
+++ src/modules/ranking/sysRank.py
@@ -14,18 +14,18 @@ class sysRank(commands.Cog):
         Log.info("🧰 Ranking system has been loaded")
     
     @commands.Cog.listener()
-    async def on_message(self, message):
+    async def on_message(self, ctx):
         try:
-            if message.author.bot:
+            if ctx.author.bot:
                 return
 
-            if message.guild is None:
+            if ctx.guild is None:
                 return
 
             try:
-                user = message.author
+                user = ctx.author
                 userID = user.id
-                guild = message.guild
+                guild = ctx.guild
                 guildID = guild.id
 
                 if not Saver.fetch(f"SELECT * FROM ranking WHERE userID = {userID} AND guildID = {guildID}"):
@@ -33,8 +33,26 @@ class sysRank(commands.Cog):
                     pass
 
                 oldXP = Saver.fetch(f"SELECT xp FROM ranking WHERE userID = {userID} AND guildID = {guildID}")
+                oldLevel = Saver.fetch(f"SELECT level FROM ranking WHERE userID = {userID} AND guildID = {guildID}")
                 xpWin = random.randint(1, 15)
                 newXP = oldXP + xpWin
+
+                nextLevelXP = 5 * (oldLevel ** 2) + 10 * oldLevel + 10
+
+                if newXP > nextLevelXP:
+                    newLevel = oldLevel + 1
+
+                    message = f"Congratulations {user.mention}, you have leveled up to level `{newLevel}`!"
+                    embed = disnake.Embed(
+                        title="🎉 Level Up",
+                        description=message,
+                        color=disnake.Color.blurple()
+                        )
+                    await ctx.send(embed=embed, delete_after=10)
+
+                    Saver.save(f"UPDATE ranking SET level = {newLevel} WHERE userID = {userID} AND guildID = {guildID}")
+                    Log.log(f"LEVEL on {guildID} user {userID} [+] {oldLevel} -> {newLevel}")
+                    pass
 
                 Saver.save(f"UPDATE ranking SET xp = {newXP} WHERE userID = {userID} AND guildID = {guildID}")
                 Log.log(f"XP on {guildID} user {userID} [+] {xpWin} -> {newXP}")
