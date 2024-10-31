cb61d9f18bab883a9a031da2850591145eb4303d
FlavianJost <68293782+FlavianJost@users.noreply.github.com>
2024-10-31 16:53:39 +0100

    Add baltop command

---
+++ src/modules/economy/baltop.py
@@ -0,0 +1,39 @@
+import disnake
+from disnake.ext import commands
+from src.utils.error import error_embed as error
+from src.utils.logger import Log
+from src.utils.saver import Saver
+
+class Baltop(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+
+    @commands.Cog.listener()
+    async def on_ready(self):
+        Log.info('🔩 /baltop has been loaded')
+        pass
+
+    @commands.slash_command(name="top_balance", description="See the top 10 users with the highest balances")
+    async def top_balance(self, ctx):
+            try:
+                guildID = ctx.guild.id
+                # Requête pour obtenir le top 10 des utilisateurs avec les balances les plus élevées dans la guilde
+                top_users = Saver.fetch(f"SELECT userID, coins FROM economy WHERE guildID = {guildID} ORDER BY coins DESC LIMIT 10")
+                
+                # Préparation du message d'affichage
+                message = "\n".join([f"<@{user[0]}> : `{user[1]}` coins" for user in top_users])
+                embed = disnake.Embed(
+                    title="🏆 Top 10 Balances 🏆",
+                    description=message if message else "No users found.",
+                    color=disnake.Color.gold()
+                )
+                await ctx.send(embed=embed)
+
+            except Exception as e:
+                embed = error(e)
+                Log.error("Failed to execute /top_balance")
+                Log.error(e)
+                await ctx.send(embed=embed)
+
+def setup(bot):
+    bot.add_cog(Baltop(bot))
