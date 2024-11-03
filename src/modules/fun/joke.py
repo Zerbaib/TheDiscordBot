9f62c0f5bc6b66c508dced7a6960a902a2f22bac
FlavianJost <68293782+FlavianJost@users.noreply.github.com>
2024-11-03 18:00:59 +0100

    Ajout de la commande joke et de la commande userinfo

---
+++ src/modules/fun/joke.py
@@ -0,0 +1,33 @@
+import random
+from disnake.ext import commands
+from src.utils.logger import Log
+
+
+class Joke(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+        self.jokes = [
+    "Why did the scarecrow win an award? Because he was outstanding in his field!",
+    "I told my wife she was drawing her eyebrows too high. She looked surprised.",
+    "Why don’t scientists trust atoms? Because they make up everything!",
+    "I would tell you a construction pun, but I’m still working on it.",
+    "Have you heard about the restaurant on the moon? Great food, no atmosphere.",
+    "Did you hear about the mathematician who’s afraid of negative numbers? He'll stop at nothing to avoid them!",
+    "Parallel lines have so much in common… it’s a shame they’ll never meet.",
+    "What do you call fake spaghetti? An impasta!",
+    "Want to hear a joke about paper? Never mind, it’s tearable.",
+    "I only know 25 letters of the alphabet. I don’t know y.",
+]
+
+    @commands.Cog.listener()
+    async def on_ready(self):
+        Log.info('🔩 /joke has been loaded')
+        pass
+
+    @commands.slash_command(name="joke", description="Get a random joke in English!")
+    async def joke(self, ctx):
+        random_joke = random.choice(self.jokes)
+        await ctx.send(random_joke)
+
+def setup(bot):
+    bot.add_cog(Joke(bot))
