fd969adbf4a277bbd7afac9f3a084ceb05aa40a8
Hugo Hennetin <hugo.hennetin@proton.me>
2024-12-17 22:22:02 +0100

    Ajout d'un système de mise à jour de l'expérience et du grade des utilisateurs en fonction de leur activité dans le canal vocal

--- src/modules/ranking/sysRank.py
+++ src/modules/ranking/sysRank.py
@@ -21,6 +21,61 @@ class sysRank(commands.Cog):
         pass
 
     @commands.Cog.listener()
+    async def on_voice_state_update(self, member, before, after):
+        try:
+            guild = member.guild
+
+            if not Saver.fetch(f"SELECT * FROM ranking WHERE userID = {member.id} AND guildID = {guild.id}"):
+                Saver.save(f"INSERT IGNORE INTO ranking (userID, guildID, xp, level, rate) VALUES ({member.id}, {guild.id}, 0, 0, {rateLimitXpDaily})")
+                pass
+
+            oldRate = Saver.fetch(f"SELECT rate FROM ranking WHERE userID = {member.id} AND guildID = {guild.id}")[0][0]
+            if oldRate == None:
+                Saver.save(f"UPDATE ranking SET rate = {rateLimitXpDaily} WHERE userID = {member.id} AND guildID = {guild.id}")
+                oldRate = rateLimitXpDaily
+            while True:
+                rate = oldRate - 5
+                xp = Saver.fetch(f"SELECT xp FROM ranking WHERE userID = {member.id} AND guildID = {guild.id}")[0][0]
+                newXP = xp + 5
+                Saver.save(f"UPDATE ranking SET xp = {newXP} WHERE userID = {member.id} AND guildID = {guild.id}")
+                Saver.save(f"UPDATE ranking SET rate = {rate} WHERE userID = {member.id} AND guildID = {guild.id}")
+
+                highest_grade = None
+                for grade, value in rankGrade.items():
+                    if newXP >= value:
+                        highest_grade = grade
+                if highest_grade:
+                    oldGrade = Saver.fetch(f"SELECT grade FROM ranking WHERE userID = {member.id} AND guildID = {guild.id}")[0][0]
+                    if oldGrade != highest_grade:
+                        Saver.save(f"UPDATE ranking SET grade = '{highest_grade}' WHERE userID = {member.id} AND guildID = {guild.id}")
+
+                        with open(emojiFile, 'r') as f:
+                            rankGradeEmoji = load(f)
+
+                        liaison_name = tableLiaison.get(highest_grade)
+                        if liaison_name:
+                            emoji_id = rankGradeEmoji.get(liaison_name)
+                        else:
+                            Log.warn(f"Failed to get emoji id {highest_grade}")
+
+                        mess = f"Congratulations {member.mention} :fire:, you have been promoted to grade **{highest_grade}** <:{liaison_name}:{emoji_id}> !"
+                        await member.send(mess)
+                        Log.log(f"GRADE on {guild.id} user {member.id} [+] {oldGrade} -> {highest_grade}")
+
+                if rate <= 0:
+                    rate = 0
+                    Log.log(f"RATE LIMIT on {guild.id} user {member.id} [+] {oldRate} -> {rate}")
+                    break
+                Log.log(f"XP on {guild.id} user {member.id} [+] 5 -> {newXP}")
+                await asyncio.sleep(60)
+                if member.voice is None:
+                    break
+        except Exception as e:
+            Log.warn("Failed to add XP on voice channel")
+            Log.warn(e)
+            return
+
+    @commands.Cog.listener()
     async def resetRateDaly(self):
         try:
             await self.bot.wait_until_ready()
