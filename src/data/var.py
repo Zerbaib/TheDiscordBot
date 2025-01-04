d8bd432490219ceb8ad9bbb93d20709f60dbea83
Hugo Hennetin <hugo.hennetin@proton.me>
2025-01-04 03:19:00 +0100

    feat: modifier la fonction get_rank_info_config pour retourner le premier élément des données de rang

--- src/data/var.py
+++ src/data/var.py
@@ -80,7 +80,7 @@ def get_rank_info_config(index):
     from json import load
     with open(files["rank"], 'r') as f:
         dataRankFiles = load(f)
-    return dataRankFiles.get(index)
+    return dataRankFiles.get(index)[0]
 
 emojisID = {
     "bronze1": 1318555597993410600,
