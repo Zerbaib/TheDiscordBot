634af645646a739c734981fdfb24b35827698b61
Hugo Hennetin <hugo.hennetin@proton.me>
2025-01-06 03:23:23 +0100

    feat: ajouter la génération de bannières de bienvenue pour les nouveaux membres

--- src/modules/logging/members.py
+++ src/modules/logging/members.py
@@ -2,6 +2,8 @@ import disnake
 from disnake.ext import commands
 from src.utils.logger import Log
 from src.utils.saver import Saver
+from PIL import Image, ImageChops, ImageDraw, ImageFont
+from src.data.var import files, folders
 
 
 def get_guild_config(guild_id):
@@ -18,10 +20,55 @@ class Members(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
+    def circle(self, pfp, size=(125, 125)):
+        pfp = pfp.resize(size, Image.LANCZOS).convert("RGBA")
+        bigsize = (pfp.size[0] * 3, pfp.size[1] * 3)
+        mask = Image.new('L', bigsize, 0)
+        draw = ImageDraw.Draw(mask)
+        draw.ellipse((0, 0) + bigsize, fill=255)
+        mask = mask.resize(pfp.size, Image.LANCZOS)
+        mask = ImageChops.darker(mask, pfp.split()[-1])
+        pfp.putalpha(mask)
+        return pfp
+    
+    def gen_banner(self, member):
+        try:
+            banner = Image.open(files["join_banner"])
+            pfp = member.avatar.with_size(125)
+            pfp = self.circle(pfp)
+            banner.paste(pfp, (25, 25), pfp)
+            banner.save(files["join_banner_finished"])
+            draw = ImageDraw.Draw(banner)
+            font = ImageFont.truetype(files["police"], 20)
+            draw.text((160, 60), member.display_name, font=font, fill="black")
+            draw.text((160, 90), "Welcome to the server!", font=font, fill="black")
+            return True
+        except Exception as e:
+            Log.error(e)
+            return False
+
     @commands.Cog.listener()
     async def on_ready(self):
         Log.info('🧰 Members log has been loaded')
         pass
+
+    @commands.Cog.slash_command(name="members", description="Check the number of members in the server")
+    async def members(self, inter):
+        try:
+            await inter.response.defer()
+            guild = inter.guild
+            members = len(guild.members)
+            if self.gen_banner(inter.author):
+                await inter.channel.send(file=disnake.File(files["join_banner_finished"]))
+            
+            embed = disnake.Embed(
+                title="Members",
+                description=f"The server has {members} members.",
+                color=disnake.Color.blue()
+            )
+            await inter.edit_original_response(embed=embed)
+        except Exception as e:
+            Log.error(e)
 
     @commands.Cog.listener()
     async def on_member_join(self, member):
@@ -30,6 +77,9 @@ class Members(commands.Cog):
         if get_guild_config(guild.id):
             join_channel = guild.get_channel(get_guild_config(guild.id)[4])
             if join_channel:
+                if self.gen_banner(member):
+                    await join_channel.send(file=disnake.File(files["join_banner_finished"]))
+                
                 embed = disnake.Embed(
                     title='Member Joined',
                     description=f'{member.mention} has joined the server!',
