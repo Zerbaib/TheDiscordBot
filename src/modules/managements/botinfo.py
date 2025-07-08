94ada298b49915a19e8fd3db4c5c4d75456fee0a
Hugo Hennetin <hugo.hennetin@proton.me>
2025-07-08 23:50:52 +0200

    fix: corriger l'affichage du propriétaire dans la commande /botinfo

--- src/modules/managements/botinfo.py
+++ src/modules/managements/botinfo.py
@@ -34,7 +34,7 @@ class BotInfo(commands.Cog):
             embed.set_thumbnail(url=bot.user.avatar.url)
             embed.add_field(name="Bot ID", value=bot.user.id, inline=True)
             embed.add_field(name="Bot Version", value=version, inline=True)
-            embed.add_field(name="Owner", value=owner.mention, inline=True)
+            embed.add_field(name="Owner", value=owner.mention, inline=False)
             embed.add_field(name="Servers", value=len(bot.guilds), inline=True)
             embed.add_field(name="Users", value=len(bot.users), inline=True)
             embed.add_field(name="Slash Commands", value=len(bot.slash_commands), inline=True)
